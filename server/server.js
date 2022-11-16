48bb9eaadb96d79ea936c59b9d6975e9dc1c9b84
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-11-15 21:39:30 -0500

    added packages

--- server/server.js
+++ server/server.js
@@ -22,13 +22,13 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
- // app.use(express.static(path.join(__dirname, '../client/build')));
+ app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-/*app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-*/
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
